228acf7467128c7dcad4d05594dfff7d065241e7
Gustavo Jesinski <gustavojesinskib@gmail.com>
2023-12-03 15:19:41 -0300

    a little bit of cleaning

--- src/days/3/part-one/index.ts
+++ src/days/3/part-one/index.ts
@@ -9,7 +9,6 @@ let accumulator = 0;
 for (let y = 0; y < lines.length; y++) {
   const regex = RegExp(/\d+/, "g");
   const numbers = lines[y].matchAll(regex);
-  // [ '467', '114' ]
 
   if (!numbers) {
     continue;
@@ -19,65 +18,51 @@ for (let y = 0; y < lines.length; y++) {
   let indexSearching = 0;
   let shouldSum = false;
   while ((array1 = regex.exec(lines[y])) !== null) {
-    console.log("processing number...", array1[0]);
     shouldSum = false;
     for (let i = 0; i < array1[0].length; i++) {
-      // for (const digit of array1[0]) {
       const x = lines[y].indexOf(array1[0], indexSearching) + i;
 
-      // indexSearching = x;
       if (lines[y - 1] && lines[y - 1][x - 1])
         if (lines[y - 1][x - 1].match(symbolsRegex) != null) {
-          console.log(y, "ok1");
           shouldSum = true;
         }
 
       if (lines[y - 1] && lines[y - 1][x])
         if (lines[y - 1][x].match(symbolsRegex) != null) {
-          console.log(lines[y - 1][x].match(symbolsRegex));
-          console.log(y, "ok2");
           shouldSum = true;
         }
 
       if (lines[y - 1] && lines[y - 1][x + 1])
         if (lines[y - 1][x + 1].match(symbolsRegex) != null) {
-          console.log(y, "ok3");
           shouldSum = true;
         }
 
       if (lines[y] && lines[y][x - 1])
         if (lines[y][x - 1].match(symbolsRegex) != null) {
-          console.log(y, "ok4");
           shouldSum = true;
         }
 
       if (lines[y] && lines[y][x + 1])
         if (lines[y][x + 1].match(symbolsRegex) != null) {
-          console.log(y, "ok5");
           shouldSum = true;
         }
 
       if (lines[y + 1] && lines[y + 1][x - 1])
         if (lines[y + 1][x - 1].match(symbolsRegex) != null) {
-          console.log(y, "ok6");
           shouldSum = true;
         }
 
       if (lines[y + 1] && lines[y + 1][x])
         if (lines[y + 1][x].match(symbolsRegex) != null) {
-          console.log(y, "ok7");
           shouldSum = true;
         }
 
       if (lines[y + 1] && lines[y + 1][x + 1])
         if (lines[y + 1][x + 1].match(symbolsRegex) != null) {
-          console.log(y, "ok8");
           shouldSum = true;
         }
     }
 
-    console.log("finished processing number...", array1[0]);
-    console.log("found adjacent symbol?", shouldSum);
     indexSearching = regex.lastIndex;
     if (shouldSum) {
       accumulator += +array1[0];
--- src/days/3/part-two/index.ts
+++ src/days/3/part-two/index.ts
@@ -2,130 +2,83 @@ import { readFileSync } from "fs";
 import path from "path";
 
 const inputFile = readFileSync(path.join(__dirname, "input.txt"), "utf-8");
-const lines = inputFile.split("\n");
+
 type NumberType = {
-  number: string;
+  number: number;
   startingIndex: number;
   endingIndex: number;
 };
 
 const numbers = getNumbers();
 
-console.log(numbers);
-
 const asterisk = /\*|\n/g;
 let accumulator = 0;
 
 let matches;
-let line = 0;
 let lastIndex = 0;
 
 // Assuming that every line has the same length
 const lineLength = inputFile.indexOf("\n");
+
 while ((matches = asterisk.exec(inputFile)) != null) {
   const adjacentNumbers = new Set<NumberType>();
-  // const adjacentNumbers: NumberType[] = [];
   if (matches[0] == "\n") {
-    line++;
     continue;
   }
   const currentIndex = inputFile.indexOf(matches[0], lastIndex);
-  console.log("line", line, currentIndex);
+
   lastIndex = asterisk.lastIndex;
 
-  const topLeft = currentIndex - lineLength - 2;
-  console.log("top-left", inputFile.charAt(topLeft));
-  let brea = checkSomeshit(topLeft, adjacentNumbers);
-  if (brea) {
-    continue;
-  }
+  const positions: { [key: string]: number } = {
+    topLeft: currentIndex - lineLength - 2,
+    top: currentIndex - lineLength - 1,
+    topRight: currentIndex - lineLength,
+    left: currentIndex - 1,
+    right: currentIndex + 1,
+    bottomLeft: currentIndex + lineLength,
+    bottom: currentIndex + lineLength + 1,
+    bottomRight: currentIndex + lineLength + 2,
+  };
 
-  const top = currentIndex - lineLength - 1;
-  console.log("top", inputFile.charAt(top));
-  brea = checkSomeshit(top, adjacentNumbers);
-  if (brea) {
-    continue;
-  }
+  for (const position in positions) {
+    const charIndex = positions[position];
+    if (!isNaN(+inputFile.charAt(charIndex))) {
+      const num = numbers.find(
+        (number) =>
+          number.startingIndex <= charIndex && charIndex <= number.endingIndex
+      );
 
-  const topRight = currentIndex - lineLength;
-  console.log("top-right", inputFile.charAt(topRight));
-  brea = checkSomeshit(topRight, adjacentNumbers);
-  if (brea) {
-    continue;
-  }
+      if (num) {
+        adjacentNumbers.add(num);
+        if (adjacentNumbers.size > 1) {
+          let product = 1;
 
-  const left = currentIndex - 1;
-  console.log("left", inputFile.charAt(left));
-  brea = checkSomeshit(left, adjacentNumbers);
-  if (brea) {
-    continue;
-  }
+          adjacentNumbers.forEach((value) => {
+            product = product * value.number;
+          });
 
-  const right = currentIndex + 1;
-  console.log("right", inputFile.charAt(right));
-  brea = checkSomeshit(right, adjacentNumbers);
-  if (brea) {
-    continue;
-  }
-
-  const bottomLeft = currentIndex + lineLength;
-  console.log("bottom-left", inputFile.charAt(bottomLeft));
-  brea = checkSomeshit(bottomLeft, adjacentNumbers);
-  if (brea) {
-    continue;
-  }
-
-  const bottom = currentIndex + lineLength + 1;
-  console.log("bottom", inputFile.charAt(bottom));
-  brea = checkSomeshit(bottom, adjacentNumbers);
-  if (brea) {
-    continue;
-  }
-
-  const bottomRight = currentIndex + lineLength + 2;
-  console.log("bottom-right", inputFile.charAt(bottomRight));
-  brea = checkSomeshit(bottomRight, adjacentNumbers);
-  if (brea) {
-    continue;
-  }
-}
-console.log({ expectedResult: 78915902, result: accumulator });
-
-function checkSomeshit(top: number, adjacentNumbers: Set<NumberType>) {
-  if (!isNaN(+inputFile.charAt(top))) {
-    const num = numbers.find(
-      (number) => number.startingIndex <= top && top <= number.endingIndex
-    );
-    if (num) {
-      adjacentNumbers.add(num);
-      if (adjacentNumbers.size > 1) {
-        let product = 1;
-
-        console.log("product", adjacentNumbers.values());
-        adjacentNumbers.forEach((value) => {
-          product = product * +value.number;
-        });
-
-        accumulator += product;
-        return true;
+          accumulator += product;
+          break;
+        }
       }
     }
   }
-  return false;
 }
+console.log({ expectedResult: 78915902, result: accumulator });
 
 function getNumbers() {
   const numbers = [];
   const digits = /\d+/g;
   let arr;
-  let sIndex = 0;
+  let lastIndex = 0;
   while ((arr = digits.exec(inputFile)) != null) {
+    const startingIndex = inputFile.indexOf(arr[0], lastIndex);
     numbers.push({
-      number: arr[0],
-      startingIndex: inputFile.indexOf(arr[0], sIndex),
-      endingIndex: inputFile.indexOf(arr[0], sIndex) + arr[0].length - 1,
+      number: +arr[0],
+      startingIndex: startingIndex,
+      endingIndex: startingIndex + arr[0].length - 1,
     });
-    sIndex = digits.lastIndex;
+    lastIndex = digits.lastIndex;
   }
 
   return numbers;
