0320a61a493086507aace87793c33d9ae7c9c429
Gustavo Jesinski <gustavojesinskib@gmail.com>
2023-12-05 06:33:44 -0300

    removed use of map

--- src/days/5/part-one/index.ts
+++ src/days/5/part-one/index.ts
@@ -6,43 +6,37 @@ const lines = inputFile.split("\n\n");
 
 console.log(lines);
 
+type SourceToDestination = {
+  destination: number;
+  source: number;
+  range: number;
+  difference: number;
+};
+
 const seeds = lines[0]
   .split(": ")[1]
   .split(" ")
   .map((seed) => +seed);
 // console.log(seeds);
 
-const seedToSoil = getRangeMap(1);
-const soilToFertilizer = getRangeMap(2);
-const fertilizerToWater = getRangeMap(3);
-const waterToLight = getRangeMap(4);
-const lightToTemperature = getRangeMap(5);
-const temperatureToHumidity = getRangeMap(6);
-const humidityToLocation = getRangeMap(7);
+const seedToSoil = getMap(1);
+const soilToFertilizer = getMap(2);
+const fertilizerToWater = getMap(3);
+const waterToLight = getMap(4);
+const lightToTemperature = getMap(5);
+const temperatureToHumidity = getMap(6);
+const humidityToLocation = getMap(7);
 
 let lowestLocation = Infinity;
 
 for (const seed of seeds) {
-  let soil: number;
-  let fertilizer: number;
-
-  if (seedToSoil.has(seed)) {
-    soil = seedToSoil.get(seed) as number;
-  } else {
-    soil = seed;
-  }
-
-  if (soilToFertilizer.has(soil)) {
-    fertilizer = soilToFertilizer.get(soil) as number;
-  } else {
-    fertilizer = soil;
-  }
-
-  const water = fertilizerToWater.get(fertilizer) || fertilizer;
-  const light = waterToLight.get(water) || water;
-  const temperature = lightToTemperature.get(light) || light;
-  const humidity = temperatureToHumidity.get(temperature) || temperature;
-  const location = humidityToLocation.get(humidity) || humidity;
+  const soil = getDestination(seedToSoil, seed);
+  const fertilizer = getDestination(soilToFertilizer, soil);
+  const water = getDestination(fertilizerToWater, fertilizer);
+  const light = getDestination(waterToLight, water);
+  const temperature = getDestination(lightToTemperature, light);
+  const humidity = getDestination(temperatureToHumidity, temperature);
+  const location = getDestination(humidityToLocation, humidity);
   lowestLocation = Math.min(lowestLocation, location);
   console.log(
     `Seed ${seed}, soil ${soil}, fertilizer ${fertilizer}, water ${water}, light ${light}, temperature ${temperature}, humidity ${humidity}, location ${location}`
@@ -51,23 +45,31 @@ for (const seed of seeds) {
 
 console.log({ expectedResult: 35, result: lowestLocation });
 
-function getRangeMap(line: number) {
-  const sourceToDestination = new Map<number, number>();
-  lines[line]
+function getMap(line: number) {
+  return lines[line]
     .split(":\n")[1]
     .split("\n")
-    .forEach((line) => {
+    .map((line) => {
       const numbers = line.split(" ");
       const payload = {
         destination: +numbers[0],
         source: +numbers[1],
         range: +numbers[2],
-        difference: +numbers[1] - +numbers[0],
+        difference: +numbers[0] - +numbers[1],
       };
-
-      for (let i = 0; i < payload.range; i++) {
-        sourceToDestination.set(payload.source + i, payload.destination + i);
-      }
+      return payload;
     });
-  return sourceToDestination;
+}
+
+function getDestination(
+  sourceToDestination: SourceToDestination[],
+  source: number
+): number {
+  sourceToDestination.forEach((entry) => {
+    if (source >= entry.source && source < entry.source + entry.range) {
+      source = source + entry.difference;
+    }
+  });
+
+  return source;
 }
