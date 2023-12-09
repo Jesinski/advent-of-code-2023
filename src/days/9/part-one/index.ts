316399411abbb02207f04e22157b9c9f4f11aaf6
Gustavo Jesinski <gustavojesinskib@gmail.com>
2023-12-09 06:57:06 -0300

    day 9 p1 done, 26minutes

---
+++ src/days/9/part-one/index.ts
@@ -0,0 +1,27 @@
+import { readFileSync } from "fs";
+import path from "path";
+
+const inputFile = readFileSync(path.join(__dirname, "input.txt"), "utf-8");
+const lines = inputFile.split("\n");
+let accumulator = 0;
+
+for (const line of lines) {
+  const rows: number[][] = [];
+  const numbers = line.split(" ").map((num) => +num);
+  rows.push(numbers);
+
+  while (!rows[rows.length - 1].every((value) => value === 0)) {
+    const nextLine: number[] = [];
+    for (let i = 0; i < rows[rows.length - 1].length - 1; i++) {
+      nextLine.push(rows[rows.length - 1][i + 1] - rows[rows.length - 1][i]);
+    }
+    rows.push(nextLine);
+  }
+
+  let sum = 0;
+  for (let i = 0; i < rows.length; i++) {
+    sum += rows[i][rows[i].length - 1];
+  }
+  accumulator += sum;
+}
+console.log({ expectedResult: 1974913025, result: accumulator });
