fd2bf4849cb065cecffa9cd1506cc6dd0c687209
Gustavo Jesinski <gustavojesinskib@gmail.com>
2023-12-09 07:26:40 -0300

    day 9 done

--- src/days/9/part-two/index.ts
+++ src/days/9/part-two/index.ts
@@ -5,4 +5,23 @@ const inputFile = readFileSync(path.join(__dirname, "input.txt"), "utf-8");
 const lines = inputFile.split("\n");
 let accumulator = 0;
 
-console.log({ expectedResult: 0, result: accumulator });
+for (const line of lines) {
+  const rows: number[][] = [];
+  const numbers = line.split(" ").map((num) => +num);
+  rows.push(numbers);
+
+  while (!rows[rows.length - 1].every((value) => value === 0)) {
+    const nextLine: number[] = [];
+    for (let i = 0; i < rows[rows.length - 1].length - 1; i++) {
+      nextLine.push(rows[rows.length - 1][i + 1] - rows[rows.length - 1][i]);
+    }
+    rows.push(nextLine);
+  }
+
+  let sum = 0;
+  for (let i = rows.length - 1; i >= 0; i--) {
+    sum = rows[i][0] - sum;
+  }
+  accumulator += sum;
+}
+console.log({ expectedResult: 884, result: accumulator });
