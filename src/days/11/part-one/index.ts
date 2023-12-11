6564aed8d07be19a1705c7caa84211ac0eac3dfa
Gustavo Jesinski <gustavojesinskib@gmail.com>
2023-12-11 19:31:34 -0300

    hm

--- src/days/11/part-one/index.ts
+++ src/days/11/part-one/index.ts
@@ -47,18 +47,19 @@ printGrid(grid);
 console.log({ expectedResult: 0, result: 0 });
 
 // find all galaxies (not wokring)
-for (let x = 0; x < xLength; x++) {
-  for (let y = 0; y < yLength; y++) {
-    if (grid[x][y] != ".") {
-      galaxies.push({
-        id: grid[x][y],
-        y: x,
-        x: y,
-        distance: new Map<string, number>(),
-      });
-    }
-  }
-}
+// for (let x = 0; x < xLength; x++) {
+//   for (let y = 0; y < yLength; y++) {
+//     if (grid[x][y] != ".") {
+//       galaxies.push({
+//         id: grid[x][y],
+//         y: x,
+//         x: y,
+//         distance: new Map<string, number>(),
+//       });
+//     }
+//   }
+// }
+galaxies.push({ id: "1", y: 0, x: 4, distance: new Map() });
 console.log({ galaxies });
 
 const DIRECTIONS: { [key: string]: [number, number] } = {
@@ -67,24 +68,63 @@ const DIRECTIONS: { [key: string]: [number, number] } = {
   LEFT: [0, -1],
   RIGHT: [0, 1],
 } as const;
-
+let unvisitedNodes: number[][] = [];
 let visited: string[] = [];
 let steps = 0;
 for (const galaxy of galaxies) {
   const startingPos = [galaxy.y, galaxy.x];
   visited = [];
   steps = 0;
+  unvisitedNodes = [];
 
-  runGrid(startingPos);
-  printGrid(grid);
+  unvisitedNodes.push(...getUnvisited(startingPos));
+  console.log(unvisitedNodes);
+
+  while (unvisitedNodes.length > 0) {
+    const next = unvisitedNodes.shift() as unknown as number[];
+    steps++;
+    grid[next[0]][next[1]] = "V";
+    printGrid(grid);
+
+    visited.push(String(next[0] + String(next[1])));
+    unvisitedNodes.push(...getUnvisited(next));
+  }
 
   console.log("galaxy", galaxy.id, steps);
+}
+
+// function visit(pos: number[]) {
+//   while (pos.length > 0) {
+//     const next = pos.pop();
+//     steps++;
+//     visited.push(String(next[0] + String(next[1])));
+//     pos.push(...getUnvisited(next));
+//   }
+//   console.log();
+// }
+
+function getUnvisited(pos: number[]): number[][] {
+  const nodes: number[][] = [];
+  const up = DIRECTIONS.UP;
+  if (canVisit(pos, up)) nodes.push([pos[0] + up[0], pos[1] + up[1]]);
+
+  const down = DIRECTIONS.DOWN;
+  if (canVisit(pos, down)) nodes.push([pos[0] + down[0], pos[1] + down[1]]);
+
+  const left = DIRECTIONS.LEFT;
+  if (canVisit(pos, left)) nodes.push([pos[0] + left[0], pos[1] + left[1]]);
+
+  const right = DIRECTIONS.RIGHT;
+  if (canVisit(pos, right)) nodes.push([pos[0] + right[0], pos[1] + right[1]]);
+
+  return nodes;
 }
 
 function runGrid(pos: number[] | undefined) {
   while (pos != undefined) {
     visited.push(String(pos[0] + String(pos[1])));
-    // grid[pos[0]][pos[1]] = "V";
+    grid[pos[0]][pos[1]] = "V";
+    printGrid(grid);
     steps++;
     pos = getNextPos(pos);
   }
