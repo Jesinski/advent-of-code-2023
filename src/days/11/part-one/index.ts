72150dd4fcc8a455e5bb8b575809246103bf697a
Gustavo Jesinski <gustavojesinskib@gmail.com>
2023-12-11 14:44:30 -0300

    going

--- src/days/11/part-one/index.ts
+++ src/days/11/part-one/index.ts
@@ -2,34 +2,55 @@ import { readFileSync } from "fs";
 import path from "path";
 
 const inputFile = readFileSync(path.join(__dirname, "input.txt"), "utf-8");
-const lines = inputFile.split("\n");
 
 const grid: string[][] = [];
+const lines = inputFile.split("\n");
+let id = 1;
+type Galaxy = {
+  id: string;
+  x: number;
+  y: number;
+};
+const galaxies: Galaxy[] = [];
 
 for (const line of lines) {
   const row = [...line.split("")];
+  row.forEach((char, index) => {
+    if (char != ".") {
+      row[index] = String(id);
+      id++;
+    }
+  });
   grid.push(row);
-  if (row.every((char) => char === ".")) grid.push(row);
+  if (row.every((char) => char === ".")) grid.push([...row]);
 }
-
 for (let x = grid[0].length - 1; x >= 0; x--) {
-  let same = true;
-  for (let y = 0; y < grid.length; y++) {
+  let hasGalaxy = false;
+  for (let y = 0; y < grid.length - 1; y++) {
     if (grid[y][x] != ".") {
-      same = false;
+      hasGalaxy = true;
       break;
     }
   }
-
-  if (same) {
-    console.log("ok");
+  if (!hasGalaxy) {
     for (let y = 0; y < grid.length; y++) {
       grid[y].splice(x, 0, ".");
     }
+    x--;
   }
 }
-
 const xLength = grid[0].length;
 const yLength = grid.length;
 console.log({ xLength, yLength });
+console.log({ galaxies });
+printGrid(grid);
 console.log({ expectedResult: 0, result: 0 });
+
+function printGrid(grid: string[][]) {
+  let line = 0;
+  for (const row of grid) {
+    const ro = row.join("");
+    console.log(line++, "\t", ro);
+  }
+  console.log();
+}
