b8c29ff56c59d2b2b12a730ae0e201ef323c59ac
Gustavo Jesinski <gustavojesinskib@gmail.com>
2024-08-09 17:10:12 -0300

    implementing minHeap

--- src/days/17/part-one/index.ts
+++ src/days/17/part-one/index.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 import path from "path";
-
+import { MinHeap } from "./minHeap";
 const inputFile = readFileSync(path.join(__dirname, "input.txt"), "utf-8");
 const matrix = inputFile.split("\n").map((e) => e.split("").map((e) => +e));
 const directions = [
@@ -12,18 +12,20 @@ const directions = [
 function getKey(node: number[]) {
   return node.join(",");
 }
+
 const m = matrix.length;
 const n = matrix[0].length;
-// row, col, heatLoss
-const toVisit = [[0, 0, 0]];
+// row, col, heatLoss, dirX, dirY, steps
+const toVisit = new MinHeap();
+toVisit.insert([0, 0, 0, 0, 0, 0]);
 const visited = new Map();
 
-while (toVisit.length > 0) {
-  const visiting = toVisit.shift();
+while (toVisit.elements > 0) {
+  const visiting = toVisit.removeMin();
   if (!visiting) {
     continue;
   }
-  const [row, column, heatLoss] = visiting;
+  const [row, column, heatLoss, dirX, dirY, steps] = visiting;
   const key = getKey([row, column]);
   if (visited.has(key)) {
     const [lastSeenCounter] = visited.get(key);
@@ -42,16 +44,31 @@ while (toVisit.length > 0) {
         const [counter] = visited.get(newKey);
         if (counter + matrix[row][column] < counter) {
           visited.set(newKey, [counter + matrix[row][column]]);
-          toVisit.push([newRow, newColumn, counter + matrix[row][column]]);
+          toVisit.insert([
+            newRow,
+            newColumn,
+            counter + matrix[row][column],
+            dx,
+            dy,
+            steps + 1,
+          ]);
         }
       } else {
-        toVisit.push([newRow, newColumn, heatLoss + matrix[row][column]]);
+        toVisit.insert([
+          newRow,
+          newColumn,
+          heatLoss + matrix[row][column],
+          dx,
+          dy,
+          steps + 1,
+        ]);
       }
     }
   }
 }
+
 console.log(visited);
-console.log(visited.get([m - 1, n - 1].join(",")));
+// console.log(visited.get([m - 1, n - 1].join(",")));
 let accumulator = 0;
 
 console.log({ expectedResult: 0, result: accumulator });
