0ce4ca5cd1aedaae755342d92bfc2817e4af54a2
Gustavo Jesinski <gustavojesinskib@gmail.com>
2024-08-10 10:18:16 -0300

    day 17 done

--- src/days/17/part-two/index.ts
+++ src/days/17/part-two/index.ts
@@ -27,53 +27,52 @@ while (toVisit.elements > 0) {
   if (!visiting) {
     continue;
   }
-  const [heatLoss, row, column, dirRow, dirCol, steps] = visiting;
+  const [hl, r, c, dr, dc, steps] = visiting;
 
-  const key = getKey([row, column, dirRow, dirCol, steps]);
+  const key = getKey([r, c, dr, dc, steps]);
   if (visited.has(key)) {
     continue;
   }
   visited.add(key);
 
-  if (row == m - 1 && column == n - 1) {
-    accumulator = heatLoss;
+  if (r == m - 1 && c == n - 1 && steps >= 4) {
+    accumulator = hl;
     break;
   }
 
-  if (steps < 3 && (dirRow != 0 || dirCol != 0)) {
-    const newRow = row + dirRow;
-    const newCol = column + dirCol;
+  if (steps < 10 && (dr != 0 || dc != 0)) {
+    const newRow = r + dr;
+    const newCol = c + dc;
     if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n) {
       toVisit.insert([
-        heatLoss + matrix[newRow][newCol],
+        hl + matrix[newRow][newCol],
         newRow,
         newCol,
-        dirRow,
-        dirCol,
+        dr,
+        dc,
         steps + 1,
       ]);
     }
   }
 
-  for (const [ndr, ndc] of directions) {
-    if (
-      (ndr != dirRow || ndc != dirCol) &&
-      (ndr != -dirRow || ndc != -dirCol)
-    ) {
-      const newRow = row + ndr;
-      const newCol = column + ndc;
-      if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n) {
-        toVisit.insert([
-          heatLoss + matrix[newRow][newCol],
-          newRow,
-          newCol,
-          ndr,
-          ndc,
-          1,
-        ]);
+  if (steps >= 4 || (dr == 0 && dc == 0)) {
+    for (const [ndr, ndc] of directions) {
+      if ((ndr != dr || ndc != dc) && (ndr != -dr || ndc != -dc)) {
+        const newRow = r + ndr;
+        const newCol = c + ndc;
+        if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n) {
+          toVisit.insert([
+            hl + matrix[newRow][newCol],
+            newRow,
+            newCol,
+            ndr,
+            ndc,
+            1,
+          ]);
+        }
       }
     }
   }
 }
 
-console.log({ expectedResult: 742, result: accumulator });
+console.log({ expectedResult: 0, result: accumulator });
