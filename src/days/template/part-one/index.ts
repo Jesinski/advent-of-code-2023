ae17f44ac58a77ad5a1693bb279c557a3f6bd4e9
Gustavo Jesinski <gustavojesinskib@gmail.com>
2024-08-10 17:18:16 -0300

    updated template

--- src/days/template/part-one/index.ts
+++ src/days/template/part-one/index.ts
@@ -3,6 +3,7 @@ import path from "path";
 
 const inputFile = readFileSync(path.join(__dirname, "input.txt"), "utf-8");
 const lines = inputFile.split("\n");
+// eslint-disable-next-line prefer-const
 let accumulator = 0;
 
 console.log({ expectedResult: 0, result: accumulator });
