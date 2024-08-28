d14ca5126a6c4ddbc7ed5bb10e2d90a2435cca9d
Gustavo Jesinski <gustavojesinskib@gmail.com>
2024-08-28 09:13:08 -0300

    Refactoring src dir

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const { argv } = require("node:process");
 const { writeFile, mkdir, cp } = require("node:fs/promises");
 const { join } = require("node:path");
+const SESSION = process.env.SESSION;
 
 (async () => {
   const [_, __, verb, year, day] = argv; // 0 - the path, 1 - the filename
@@ -15,7 +16,7 @@ const { join } = require("node:path");
       await getInput(year, day);
       break;
     case "bp":
-      await createFolderDay(day);
+      await createFolderDay(year, day);
       await getInput(year, day);
       break;
     default:
@@ -29,8 +30,7 @@ const { join } = require("node:path");
 async function getInput(year, day) {
   const req = new Request(`https://adventofcode.com/${year}/day/${day}/input`, {
     headers: {
-      cookie:
-        "session=53616c7465645f5f3ffeb0a5ad9a80e0f26d1f58f7705fc1f19dc8c011166ba7cdd433c1db46a3d977589fedcdb5b4b37a451e77971422085a249b2ce919395a",
+      cookie: `session=${SESSION}`,
     },
   });
   const input = await fetch(req).then(async (res) => {
@@ -38,7 +38,7 @@ async function getInput(year, day) {
     return text.slice(0, text.length - 1);
   });
 
-  const path = join(__dirname, `src/days/${day}/input.txt`);
+  const path = join(__dirname, `src/${year}/days/${day}/input.txt`);
   await writeFile(path, input)
     .then(() => console.log("Input saved at", path))
     .catch((err) => console.error(err));
@@ -46,11 +46,11 @@ async function getInput(year, day) {
 // Feat: Get the each part example data for a given day
 
 // Feat: Create the boiler plate code for a given day
-async function createFolderDay(day) {
-  const rootPath = join(__dirname, "src", "days", day);
+async function createFolderDay(year, day) {
+  const rootPath = join(__dirname, "src", year, "days", day);
   await mkdir(rootPath);
 
-  const templatePathP1 = join(__dirname, "src", "days", "template");
+  const templatePathP1 = join(__dirname, "src", "template");
   await cp(templatePathP1, rootPath, { recursive: true });
 }
 
