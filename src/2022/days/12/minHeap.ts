41a3034577b4618285880b0bf72885b00e65e22c
Gustavo Jesinski <gustavojesinskib@gmail.com>
2024-08-31 15:27:56 -0300

    2022 12 damn

---
+++ src/2022/days/12/minHeap.ts
@@ -0,0 +1,86 @@
+type Node = [number, number, number];
+const LOOKUP_INDEX = 2;
+
+export class MinHeap {
+  heap: Array<Node>;
+  elements: number;
+
+  constructor() {
+    this.heap = [];
+    this.elements = 0;
+  }
+
+  insert(value: Node) {
+    if (this.elements >= this.heap.length) {
+      this.elements = this.elements + 1;
+      this.heap.push(value);
+      this._percolateUp(this.heap.length - 1);
+    } else {
+      this.heap[this.elements] = value;
+      this.elements = this.elements + 1;
+      this._percolateUp(this.elements - 1);
+    }
+  }
+
+  getMin() {
+    if (this.heap.length != 0) {
+      return this.heap[0];
+    } else {
+      return null;
+    }
+  }
+
+  removeMin() {
+    const min = this.heap[0];
+    if (this.elements > 1) {
+      this.heap[0] = this.heap[this.elements - 1];
+      this.elements = this.elements - 1;
+      this._minHeapify(0);
+      return min;
+    } else if (this.elements == 1) {
+      this.elements = this.elements - 1;
+      return min;
+    } else {
+      return null;
+    }
+  }
+
+  private _percolateUp(index: number) {
+    const parent = Math.floor((index - 1) / 2);
+    if (index <= 0) {
+      return;
+    } else if (
+      this.heap[parent][LOOKUP_INDEX] > this.heap[index][LOOKUP_INDEX]
+    ) {
+      const tmp = this.heap[parent];
+      this.heap[parent] = this.heap[index];
+      this.heap[index] = tmp;
+      this._percolateUp(parent);
+    }
+  }
+
+  private _minHeapify(index: number) {
+    const left = index * 2 + 1;
+    const right = index * 2 + 2;
+
+    let smallest = index;
+    if (
+      this.elements > left &&
+      this.heap[smallest][LOOKUP_INDEX] > this.heap[left][LOOKUP_INDEX]
+    ) {
+      smallest = left;
+    }
+    if (
+      this.elements > right &&
+      this.heap[smallest][LOOKUP_INDEX] > this.heap[right][LOOKUP_INDEX]
+    ) {
+      smallest = right;
+    }
+    if (smallest != index) {
+      const tmp = this.heap[smallest];
+      this.heap[smallest] = this.heap[index];
+      this.heap[index] = tmp;
+      this._minHeapify(smallest);
+    }
+  }
+}
---
+++ src/2022/days/12/part-one/index.ts
@@ -0,0 +1,66 @@
+import { readFileSync } from "fs";
+import path from "path";
+import { MinHeap } from "../minHeap";
+
+const inputFile = readFileSync(path.join(__dirname, "../input.txt"), "utf-8");
+const lines = inputFile.split("\n").map((e) => e.split(""));
+// eslint-disable-next-line prefer-const
+let accumulator = 0;
+
+const source: [number, number] = [-1, -1];
+const target: [number, number] = [-1, -1];
+const rows = lines.length;
+const cols = lines[0].length;
+
+for (let r = 0; r < rows; r++) {
+  for (let c = 0; c < cols; c++) {
+    if (lines[r][c] == "S") {
+      source[0] = r;
+      source[1] = c;
+      lines[r][c] = "a";
+    }
+    if (lines[r][c] == "E") {
+      target[0] = r;
+      target[1] = c;
+      lines[r][c] = "z";
+    }
+  }
+}
+
+const queue = new MinHeap();
+queue.insert([...source, 0]);
+const visited = new Set();
+const directions = [
+  [1, 0],
+  [0, -1],
+  [0, 1],
+  [-1, 0],
+];
+while (queue.elements) {
+  const node = queue.removeMin();
+  if (!node) {
+    console.log("Unreachable");
+    continue;
+  }
+  const [r, c, s] = node;
+  if (r == target[0] && c == target[1]) {
+    accumulator = s;
+    break;
+  }
+  if (visited.has(`${r},${c},${s}`)) {
+    continue;
+  }
+  visited.add(`${r},${c},${s}`);
+  for (const [dx, dy] of directions) {
+    const nr = r + dx;
+    const nc = c + dy;
+    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
+      const oldChar = lines[r][c].charCodeAt(0);
+      const char = lines[nr][nc].charCodeAt(0);
+      if (char <= oldChar + 1) {
+        queue.insert([nr, nc, s + 1]);
+      }
+    }
+  }
+}
+console.log({ expectedResult: 440, result: accumulator });
